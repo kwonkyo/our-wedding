bdf1371a5b6633967179e93952ed6d7d1180337d
kwonkyo <victor.kwon1@gmail.com>
2019-12-17 21:22:20 -0700

    move monogram to App

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import { Home } from './Home.js';
 function App() {
   return (
     <div className="App">
+      <div className="Monogram">
+        <img src="android-chrome-512x512.png"/>
+      </div>
       <Home></Home>
     </div>
   );
--- src/Home.js
+++ src/Home.js
@@ -2,14 +2,10 @@ import React from 'react';
 import './Home.css';
 import ReactFitText from 'react-fittext';
 
-
 export class Home extends React.Component {
     render() {
         return (
             <div className="Home">
-                <div className="Monogram">
-                    <img src="android-chrome-512x512.png"/>
-                </div>
                 <div className="Banner">
                     <ReactFitText compressor={.4}>
                         <h1 className="PrimaryText animated animatedFadeInUp fadeInUp">
