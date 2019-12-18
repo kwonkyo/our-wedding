c729b7846fc8f5d699078899aacdb7022fc08dfe
kwonkyo <victor.kwon1@gmail.com>
2019-12-17 23:43:32 -0700

    change copyright declaration

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ function App() {
         </Switch>
       </Router>
       <div className="Footer">
-        This website is designed by © Hee Hyun and Victor.
+        This website was made by © Hee Hyun and Victor.
       </div>
     </div>
   );
