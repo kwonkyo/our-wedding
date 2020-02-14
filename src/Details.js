d9c18b41be26b3a5e846aa8c5631c7c19d8de477
kwonkyo <victor.kwon1@gmail.com>
2020-02-13 18:24:05 -0700

    Update details to single venue information

--- src/Details.js
+++ src/Details.js
@@ -8,7 +8,7 @@ import {
 } from 'react-bootstrap';
 
 export class Details extends React.Component {
-    GoogleMapLink = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2373.913464428593!2d-113.52014558398015!3d53.487875980008994!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x53a01f446b744621%3A0x8673f4be8dab2514!2sMcLaurin%20Memorial%20Baptist%20Church!5e0!3m2!1sen!2sca!4v1576902671004!5m2!1sen!2sca";
+    GoogleMapLink = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2370.974743619782!2d-113.49170358397834!3d53.540365180020075!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x53a02244379696db%3A0xd6ea79f64a1b5361!2sFairmont%20Hotel%20Macdonald!5e0!3m2!1sen!2sca!4v1578270883703!5m2!1sen!2sca";
 
     render() {
         return (
@@ -23,29 +23,31 @@ export class Details extends React.Component {
                 <Container>
                     <Row>
                         <Col className="Ceremony">
-                            <div className="Where">
+                            <div className="When">
                                 <h3>Ceremony</h3>
-                                <p className="Time">13:00</p>
-                                <p className="Address">
-                                    McLaurin Memorial Baptist Church
-                                    <br/>
-                                    11107 51 Ave NW, Edmonton, AB T6H 0L5
-                                </p>
-                                <iframe src={this.GoogleMapLink} frameborder="0" allowfullscreen=""/>
+                                <p className="Time">13:30 - 15:00</p>
                             </div>
                         </Col>
                         <Col className="Reception">
-                            <div className="Where">
+                            <div className="When">
                                 <h3>Reception</h3>
-                                <p className="Time">17:00</p>
-                                <p className="Address">
-                                    Pampa Brazilian Steakhouse Ellerslie
-                                    <br/>
-                                    9626 Ellerslie Rd SW, Edmonton, AB T6X 0N5
-                                </p>
-                                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2377.3716154881504!2d-113.48463208398216!3d53.42606347999575!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x53a01e9bfc0b73ad%3A0x81629cbd5bcdbdac!2sPampa%20Brazilian%20Steakhouse%20Ellerslie!5e0!3m2!1sen!2sca!4v1576917976491!5m2!1sen!2sca" frameborder="0" allowfullscreen=""></iframe>
+                                <p className="Time">17:00 - 22:00</p>
                             </div>
                         </Col>
+                    </Row>
+                    <Row>
+                        <Col className="Where">
+                            <p className="Address">
+                                Fairmont Hotel Macdonald
+                                <br/>
+                                Wedgewood Room
+                                <br/>
+                                10065 100 St NW, Edmonton, AB T5J 0N6
+                            </p>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col><iframe src={this.GoogleMapLink} frameborder="0" allowfullscreen=""/></Col>
                     </Row>
                 </Container>
             </div>
