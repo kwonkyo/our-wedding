450dbff366406334e4f82a14b6a38ad487b96af9
victor.kwon@granify.com <victor.kwon1@gmail.com>
2020-08-19 19:43:38 -0600

    Make App a class

--- src/App.js
+++ src/App.js
@@ -12,42 +12,45 @@ import {
 } from "react-router-dom";
 
 
-function App() {
-  return (
-    <div className="App">
-      <Router>
-        <div className="Header">
-          <Link to="/">
-            <img className="Monogram" src="android-chrome-512x512.png"/>
-          </Link>
-          <div className="Nav">
-            <Link to="/gallery">
-              <div className="NavButton">
-                <span>Gallery</span>
-              </div>
-            </Link>
-            <Link to="/details">
-              <div className="NavButton">
-                <span>Details</span>
-              </div>
-            </Link>
-            <Link to="/rsvp">
-              <div className="NavButton RSVPButton">
-                <span>RSVP</span>
-              </div>
-            </Link>
-          </div>
-        </div>
+export class App extends React.Component  {
+    render() {
+        return (
+            <div className="App">
+                <Router>
+                    <div className="Header">
+                        <Link to="/">
+                            <img className="Monogram" src="android-chrome-512x512.png"/>
+                        </Link>
+                        <div className="Nav">
+                            <Link to="/gallery">
+                                <div className="NavButton">
+                                    <span>Gallery</span>
+                                </div>
+                            </Link>
+                            <Link to="/details">
+                                <div className="NavButton">
+                                    <span>Details</span>
+                                </div>
+                            </Link>
+                            <Link to="/rsvp">
+                                <div className="NavButton RSVPButton">
+                                    <span>RSVP</span>
+                                </div>
+                            </Link>
+                        </div>
+                    </div>
 
-        <Switch>
-          <Route exact path="/" component={Home}/>
-          <Route exact path='/rsvp' component={RSVP}/>
-          <Route exact path='/details' component={Details}/>
-          <Route exact path='/gallery' component={Gallery}/>
-        </Switch>
-      </Router>
-    </div>
-  );
+                    <Switch>
+                        <Route exact path="/" component={Home}/>
+                        <Route exact path='/rsvp' component={RSVP}/>
+                        <Route exact path='/details' component={Details}/>
+                        <Route exact path='/gallery' component={Gallery}/>
+                    </Switch>
+                </Router>
+            </div>
+        );
+    }
 }
+
 
 export default App;
