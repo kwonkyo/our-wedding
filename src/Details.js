dcbb272a2bcad38f4a556b3f8a7b88a57e09c003
victor.kwon@granify.com <victor.kwon1@gmail.com>
2020-08-22 16:52:49 -0600

    Add parking information

--- src/Details.js
+++ src/Details.js
@@ -2,13 +2,50 @@ import React from 'react';
 import './Details.css';
 import ReactFitText from 'react-fittext';
 import {
-    Container,
-    Row,
-    Col
+    Container, Row, Col, Button, Modal
 } from 'react-bootstrap';
 
 export class Details extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            showParkingInformationModal: false
+        };
+
+        this.toggleParkingInformationModal = this.toggleParkingInformationModal.bind(this);
+    }
+
+    toggleParkingInformationModal() {
+        this.setState({
+            showParkingInformationModal: !this.state.showParkingInformationModal
+        });
+    }
+
     render() {
+        const parkingInformationModal = (
+            <Modal className="ParkingInformationModal" show={this.state.showParkingInformationModal} onHide={this.toggleParkingInformationModal}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Parking</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    We understand that finding parking in downtown Edmonton is tough, and we appreciate you coming to celebrate our wedding despite these challenges!
+                    <br/><br/>
+                    We suggest a few options for parking:
+                    <br/><br/>
+                    1. If you don't mind walking a few blocks, you can click <a href="https://parking.manulifeplace.com/">here</a> to reserve a spot in the Manulife Place underground parking, which is 8 minutes walk away from the hotel.<br/>
+                    2. If you have mobility issues or need parking in the hotel grounds for any reason, please contact Hee Hyun or Victor directly, and we can book a parking spot for you in the hotel parking lot.
+                    <br/><br/>
+                    If you are familiar with the area, you can also try to find street parking or public parking in the vicinity.
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="primary" onClick={this.toggleParkingInformationModal}>
+                        Close
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        )
+
         return (
             <div className="Details">
                 <div className="Title">
@@ -29,7 +66,7 @@ export class Details extends React.Component {
                         <Col className="Reception">
                             <div className="When">
                                 <h3><i>Reception</i></h3>
-                                <p className="Time">17:00 - 20:00</p>
+                                <p className="Time">17:30 - 20:00</p>
                             </div>
                         </Col>
                     </Row>
@@ -51,8 +88,14 @@ export class Details extends React.Component {
                         <Row className="Attire">
                             <p>We'd love to see you in formal or semi-formal attire.</p>
                         </Row>
+                        <Row className="Parking">
+                            <Button variant="secondary" onClick={this.toggleParkingInformationModal}>
+                                Click here for parking details.
+                            </Button>
+                        </Row>
                     </Row>
                 </Container>
+                { parkingInformationModal }
             </div>
         );
     }
